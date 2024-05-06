efe5fc2b29e5427ac74edcaf5515283558c68aad
Raul Eduardo Luna Diaz <lunareld21@gmail.com>
2024-05-06 12:18:47 -0400

    Add files via upload

---
+++ Python intermedio/Eliminar Strng Vacio.py
@@ -0,0 +1,8 @@
+
+lista = ["a", "","", "b"]
+sin_string = []
+
+for string in lista:
+    if(string !=""):
+        sin_string.append(string)
+print(sin_string)
---
+++ Python intermedio/NORMAS avanzadas de las VARIABLES, LISTAS y TUPLAS - Python.py
@@ -0,0 +1,5 @@
+A,B,C = 0,10,True
+print(A,B,C)
+
+a = b = c = (20,100,34)
+print(a,b,c)
